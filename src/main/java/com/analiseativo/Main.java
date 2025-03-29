646d70a8facb3317a2ea2ee243b1cd1365b23a09
mikaelsfm <mikael.schwind-terceiro@cea.com.br>
2025-03-29 17:31:25 -0300

    feat: :sparkles: adiciona fluxo que permite aplicar o ativo e valores dinamicamente

--- src/main/java/com/analiseativo/Main.java
+++ src/main/java/com/analiseativo/Main.java
@@ -1,15 +1,32 @@
 package com.analiseativo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Uso: java -jar analise-ativo.jar <ATIVO> <PREÇO_VENDA> <PREÇO_COMPRA>");
-            return;
-        }
 
-        String ativo = args[0];
-        double precoVenda = Double.parseDouble(args[1]);
-        double precoCompra = Double.parseDouble(args[2]);
+        Scanner input = new Scanner(System.in);
+
+        String ativo;
+        double precoVenda;
+        double precoCompra;
+
+        if (args.length != 3) {
+            System.out.println("Qual a ação desejada? ");
+            ativo = input.nextLine();
+            System.out.println("Ação escolhida: " + ativo);
+            System.out.println("Qual o valor de venda desejado? ");
+            precoVenda = Double.parseDouble(input.nextLine());
+            System.out.println("Valor de venda: " + precoVenda);
+            System.out.println("Qual o valor de compra desejado? ");
+            precoCompra = Double.parseDouble(input.nextLine());
+            System.out.println("Valor de compra: " + precoCompra);
+            input.close();
+        } else {
+            ativo = args[0];
+            precoVenda = Double.parseDouble(args[1]);
+            precoCompra = Double.parseDouble(args[2]);
+        }
 
         MonitoraAcao monitor = new MonitoraAcao(ativo, precoVenda, precoCompra);
         monitor.start();
