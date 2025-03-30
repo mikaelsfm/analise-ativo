5f95b19765b472e92727127791e9670c51c5a1a7
mikaelsfm <mikael.schwind-terceiro@cea.com.br>
2025-03-30 20:09:03 -0300

    feat: :sparkles: adiciona throws no método getCotacao

--- src/main/java/com/analiseativo/MonitoraAcao.java
+++ src/main/java/com/analiseativo/MonitoraAcao.java
@@ -13,6 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.stream.Collectors;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MonitoraAcao {
@@ -42,10 +43,12 @@ public class MonitoraAcao {
 
                     if (cotacaoAtual >= precoVenda) {
                         System.out.println("O preço para venda está mais alto!");
-                        emailService.enviarAlerta("Alerta de Venda " + ativo, "O ativo " + ativo + " atingiu o preço de venda: R$" + cotacaoAtual);
+                        emailService.enviarAlerta("Alerta de Venda " + ativo,
+                                "O ativo " + ativo + " atingiu o preço de venda: R$" + cotacaoAtual);
                     } else if (cotacaoAtual <= precoCompra) {
                         System.out.println("O preço para compra está mais baixo!");
-                        emailService.enviarAlerta("Alerta de Compra " + ativo, "O ativo " + ativo + " atingiu o preço de compra: R$" + cotacaoAtual);
+                        emailService.enviarAlerta("Alerta de Compra " + ativo,
+                                "O ativo " + ativo + " atingiu o preço de compra: R$" + cotacaoAtual);
                     }
                 } catch (Exception e) {
                     System.err.println("Erro ao obter cotação: " + e.getMessage());
@@ -56,36 +59,45 @@ public class MonitoraAcao {
     }
 
     private double getCotacao(String ativo) throws IOException {
-        String url = "https://brapi.dev/api/quote/" + ativo + "?token=" + KEY;
-    
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("GET");
-    
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = reader.lines().collect(Collectors.joining());
-        reader.close();
-    
-        JSONObject json = new JSONObject(response);
-        JSONObject stockData = json.getJSONArray("results").getJSONObject(0);
-        double cotacao = stockData.getDouble("regularMarketPrice");
-    
-        salvaCotacaoCSV(ativo, cotacao);
-    
-        return cotacao;
+        try {
+            String url = "https://brapi.dev/api/quote/" + ativo + "?token=" + KEY;
+
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = reader.lines().collect(Collectors.joining());
+            reader.close();
+
+            JSONObject json = new JSONObject(response);
+            JSONObject stockData = json.getJSONArray("results").getJSONObject(0);
+            double cotacao = stockData.getDouble("regularMarketPrice");
+
+            salvaCotacaoCSV(ativo, cotacao);
+
+            return cotacao;
+        } catch (IOException e) {
+            System.err.println("Erro ao obter cotação: " + e.getMessage());
+            throw e;
+        } catch (JSONException e) {
+            System.err.println("Erro ao processar o JSON: " + e.getMessage());
+            throw new IOException("Erro ao processar a resposta JSON", e);
+        }
+
     }
 
     private void salvaCotacaoCSV(String ativo, double cotacao) {
-    String nomeArquivo = "src/main/resources/cotacoes.csv";
-    LocalDateTime agora = LocalDateTime.now();
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    String timestamp = agora.format(formatter);
+        String nomeArquivo = "src/main/resources/cotacoes.csv";
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String timestamp = agora.format(formatter);
 
-    try (FileWriter fw = new FileWriter(nomeArquivo, true);
-         PrintWriter pw = new PrintWriter(fw)) {
-        pw.println(timestamp + "," + ativo + "," + cotacao);
-    } catch (IOException e) {
-        System.err.println("Erro ao salvar cotação no CSV: " + e.getMessage());
+        try (FileWriter fw = new FileWriter(nomeArquivo, true);
+                PrintWriter pw = new PrintWriter(fw)) {
+            pw.println(timestamp + "," + ativo + "," + cotacao);
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar cotação no CSV: " + e.getMessage());
+        }
     }
-}
 
 }
